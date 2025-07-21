8070ea89468b0a810953417258d33e71c16de013
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-07-21 13:08:03 -0300

    refactor: clean up main.py to only handle app initialization and routing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.core.database import Base, engine, SessionLocal
-from app.models.movie import Movie
+from app.core.database import Base, engine
 from app.csv_loader import load_csv_to_db
+from app.api.routes import router as api_router
 
 app = FastAPI()
 
@@ -9,20 +9,4 @@ Base.metadata.create_all(bind=engine)
 
 load_csv_to_db("movies.csv")
 
-@app.get("/")
-def root():
-    session = SessionLocal()
-    try:
-        winners = session.query(Movie).filter(Movie.winner == True).all()
-        return [
-            {
-                "year": movie.year,
-                "title": movie.title,
-                "studios": movie.studios,
-                "producers": movie.producers,
-                "winner": movie.winner,
-            }
-            for movie in winners
-        ]
-    finally:
-        session.close()
+app.include_router(api_router)
