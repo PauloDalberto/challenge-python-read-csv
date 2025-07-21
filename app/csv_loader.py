e871b3e54950cee461fa09ee9a157ac6ac846e5d
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-07-21 11:23:47 -0300

    feat: implement CSV loader to populate database with movie data

---
+++ app/csv_loader.py
@@ -0,0 +1,28 @@
+import csv
+from app.models.movie import Movie
+from app.core.database import SessionLocal
+
+def load_csv_to_db(csv_path: str):
+    session = SessionLocal()
+    try:
+        with open(csv_path, encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=';')
+            for row in reader:
+                winner = row["winner"].strip().lower() == "yes"
+                movie = Movie(
+                    year=int(row["year"]),
+                    title=row["title"],
+                    studios=row["studios"],
+                    producers=row["producers"],
+                    winner=winner,
+                )
+                session.add(movie)
+            session.commit()
+    except FileNotFoundError:
+        print(f"Erro: Arquivo {csv_path} não encontrado.")
+        raise
+    except Exception as e:
+        print(f"Erro ao carregar dados do CSV: {e}")
+        raise
+    finally:
+        session.close()
