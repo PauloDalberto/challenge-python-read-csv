33110cdfd867ff8182bd6bb19e93544e56c8a115
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-07-23 09:38:50 -0300

    feat: add new route to main

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from app.core.database import Base, engine
-from app.csv_loader import load_csv_to_db
 from app.api.routes import router as api_router
+from app.api.csv_routes import router as api_csv_router
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-load_csv_to_db("movies.csv")
-
 app.include_router(api_router)
+app.include_router(api_csv_router)
